3b8089c396d1ce9d59928f9b3c9dfdbb53ba2976
Johan Zicola <johan.zicola@gmail.com>
2021-01-07 10:43:55 +0100

    Modify methods.sRNA.spike.in.design.step1.py to add only one fasta as input (containing miRNAs). Single output is random.fa

--- methods.sRNA.spike.in.design/methods.sRNA.spike.in.design.step1.py
+++ methods.sRNA.spike.in.design/methods.sRNA.spike.in.design.step1.py
@@ -1,68 +1,63 @@
 #methods.sRNA.spike.in.design.step1.py
-#
-#input1: mature.miRNA.seqs.top50percent file that contains sequences for most abundant miRNAs
-#output1: mature.miRNA.seqs.top50percent.fa
-#
-#input2: mature.miRNA.seqs.top50percent
-#output2: random.fa 
 
-import random
+# Input = fasta file containing miRNAs
+# Output = random.fa 
 
-#reads in mature.miRNA.seqs.top50percent file and returns the mature.miRNA.seqs.top50percent.fa file
-inFile = 'mature.miRNA.seqs.top50percent'
-ifh = open(inFile)
-inLines = ifh.readlines()
-ifh.close()
+import random, sys
 
-outFile = 'mature.miRNA.seqs.top50percent.fa'
-ofh = open(outFile,'w')
+# Provide as argument the fasta file containing the miRNAs
+ref_fasta = sys.argv[1]
 
-for line in inLines[2:]:
-    if len(line) > 0:
-        cols = line.split('\t')
-        seq = cols[0].strip()
-        name = cols[1].strip()        
-        print >>ofh,'>' + name
-        print >>ofh,seq
+fasta =  open(ref_fasta,"r")
+fasta = fasta.read()
+fasta = fasta.split("\n")
 
-ofh.close()
+list_seq = []
 
+for seq in fasta:
+    if seq[:1] != ">":
+        seq = seq.strip()
+        list_seq.append(seq)
 
-#reads in mature.miRNA.seqs.top50percent file, and 1) records frequency of each base at each position, 2) based on
-#these frequencies generates 1000 random 13mer sub-sequences (positions 4-17)
+#Remove empty line if there is
+list_seq = [_f for _f in list_seq if _f]
 
-inFile = 'mature.miRNA.seqs.top50percent'
-ifh = open(inFile)
-inLines = ifh.readlines()
-ifh.close()
-
+# Create an empty dictionary
 freqDict = {}
 
+# Create a dictionary of 21 elements (0 to 20), each element has a key (position) and a value tuple of 4 values (frequency of each
+# nucleotides
 for i in range(0,21):
     freqDict[i] = (0,0,0,0)
 
-for line in inLines[2:]:
-    cols = line.split('\t')
-    seq = cols[0].strip()
+# Retrieve only the first column of the input file (miRNA sequence) and assess the frequency of each
+# nucleotide for each position and each miRNA
+# Fill in the tuple for each element of the dictionary so that every position has 
+# 4 frequency values
+for seq in list_seq:
     for i in range(0,21):
-        if len(seq) > i:
+        if len(seq) > i: 
             if seq[i] == 'A':
                 freqDict[i] = (freqDict[i][0] + 1,freqDict[i][1],freqDict[i][2],freqDict[i][3])
             if seq[i] == 'G':
                 freqDict[i] = (freqDict[i][0],freqDict[i][1] + 1,freqDict[i][2],freqDict[i][3])
             if seq[i] == 'C':
                 freqDict[i] = (freqDict[i][0],freqDict[i][1],freqDict[i][2] + 1,freqDict[i][3])
-            if seq[i] == 'T':
+            if seq[i] == 'T' or seq[i] == 'U':
                 freqDict[i] = (freqDict[i][0],freqDict[i][1],freqDict[i][2],freqDict[i][3] + 1)
 
+
+# Create a new dictionary that will contain the chain of each nucleotides based on their frequency
 freqDict2 = {}
 
+# Then add a number of CGTA equivalent to the frequencies values given in freqDict
+# This will create a dictionary of 21 elements, each element containing x times each nucleotide 
 for pos in freqDict.keys():
     baseList = []
     A_count = freqDict[pos][0]
     G_count = freqDict[pos][1]
     C_count = freqDict[pos][2]
-    T_count = freqDict[pos][3]
+    U_count = freqDict[pos][3]
 
     for i in range(0,A_count):
         baseList.append('A')
@@ -70,15 +65,19 @@ for pos in freqDict.keys():
         baseList.append('G')
     for i in range(0,C_count):
         baseList.append('C')
-    for i in range(0,T_count):
-        baseList.append('T')
+    for i in range(0,U_count):
+        baseList.append('U')
     
     freqDict2[pos] = (baseList)
 
-
+# Redirect stdout to random.fa
 outFile = 'random.fa'
 ofh = open(outFile,'w')
 
+# Look for each elements in the dictionary freqDict2 and choose randomly one nucleotide
+# in each element, and do that for the 21 elements so that a sequence of 21 nucleotides
+# is built. The process is reiterated 1000 times and formated in fasta format
+# Only the nucleotide from 5 to 17 are displayed 
 for i in range(0,1000):
     randomSeq = ''
     for pos in freqDict2.keys():
@@ -86,7 +85,3 @@ for i in range(0,1000):
     print >>ofh,'>' + str(i)
     print >>ofh,randomSeq[4:17]
 
-    
-    
-        
-    
