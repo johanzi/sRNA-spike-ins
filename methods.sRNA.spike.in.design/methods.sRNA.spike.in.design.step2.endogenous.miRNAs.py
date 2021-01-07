e184c31f7374980cf51ff781a794c0e02c75b545
Johan Zicola <johan.zicola@gmail.com>
2021-01-07 11:21:57 +0100

    Change methods.sRNA.spike.in.design.step2.endogenous.miRNAs.py so that the input file should be provided as first argument

--- methods.sRNA.spike.in.design/methods.sRNA.spike.in.design.step2.endogenous.miRNAs.py
+++ methods.sRNA.spike.in.design/methods.sRNA.spike.in.design.step2.endogenous.miRNAs.py
@@ -1,15 +1,17 @@
-#methods.sRNA.spike.in.design.step2.endogenous.miRNAs.py
+# methods.sRNA.spike.in.design.step2.endogenous.miRNAs.py
 #
-#input: mature.miRNA.seqs.top50percent.fa
-#output: 1) folding structures calculated by RNAfold, 2) minimum free energies (MFEs) calculated by RNAfold
+# input: Fasta file containing miRNAs
+# output: 1) folding structures calculated by RNAfold, 2) minimum free energies (MFEs) calculated by RNAfold
 
 import sys, os
 
+# Get first argument
 inFile = sys.argv[1] 
 
+# Execute RNAfold command on the input fasta file
 cmd = 'cat ' + inFile + '| RNAfold -T 4 --noPS > ' + 'mature.miRNA.seqs_folded'
-
 os.system(cmd)
+
 
 # Retrieve mfes by parsing the output of RNAfold in mature.miRNA.seqs_folded
 outFile = 'mature.miRNA.seqs_mfes'
@@ -32,4 +34,3 @@ for mfe in mfeList:
     print >>ofh,mfe
 
 ofh.close()    
-
